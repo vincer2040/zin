3d2e7267c0a2ace96b508449a6c1b593b18b0f95
boreddad <ronnebaumfinances@gmail.com>
2023-12-27 15:42:23 -0500

    restart again

--- lib/lexer.mli
+++
@@ -1,6 +0,0 @@
-type t
-
-val init : string -> t
-val next_token : t -> t * Token.token option
-val pp : Format.formatter -> t -> unit
-val show : t -> string
--- lib/util.ml
+++
@@ -1,2 +0,0 @@
-
-module StringMap = Map.Make (String)
